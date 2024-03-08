355099f8a9daa5872e79cf71ed493b24c6cc436b
Marco9723 <82958588+Marco9723@users.noreply.github.com>
2024-03-08 17:51:25 +0100

    Update reconstruct.py

--- model/reconstruct.py
+++ model/reconstruct.py
@@ -10,7 +10,6 @@ from torchmetrics.audio.pesq import PerceptualEvaluationSpeechQuality as PESQ
 from torchmetrics.audio.stoi import ShortTimeObjectiveIntelligibility as STOI
 import torch
 import sys
-sys.path.append("C:/Users/marco/Documents/GitHub/Packet_Loss_Concealment_Thesis/TESI")
 from CODE.PLCmos.plc_mos import PLCMOSEstimator
 import numpy as np
 from utils import LSD
@@ -20,12 +19,10 @@ plc_mos = PLCMOSEstimator()
 
 def main():
 
-    # model_checkpoint = "C:/Users/marco/Documents/GitHub/Packet_Loss_Concealment_Thesis/TESI/CODE/lightning_logs/version_271/checkpoints/frn-epoch=126-val_loss=-8.9949.ckpt"
-    # model_checkpoint = "C:/Users/marco/Documents/GitHub/Packet_Loss_Concealment_Thesis/TESI/CODE/fine_tuning/lightning_logs/version_214/checkpoints/new-frn-epoch=97-val_loss=-4.0343.ckpt"
-    model_checkpoint = 'C:/Users/marco/Documents/GitHub/Packet_Loss_Concealment_Thesis/TESI/CODE/fine_tuning/lightning_logs/version_61/checkpoints/parcnet-epoch=88-packet_val_loss=-1.5027.ckpt'
-    audio_test_folder = Path("C:/Users/marco/Documents/GitHub/Packet_Loss_Concealment_Thesis/TESI/data/val_clean_v2")
-    trace_folder = Path("C:/Users/marco/Documents/GitHub/Packet_Loss_Concealment_Thesis/TESI/data/traces_v2")
-    output_folder = "C:/Users/marco/Documents/GitHub/Packet_Loss_Concealment_Thesis/TESI/CODE/predictions"
+    model_checkpoint = '../model.ckpt'
+    audio_test_folder = Path("../val_clean_v2")
+    trace_folder = Path("../traces_v2")
+    output_folder = Path("../predictions")
     
     # Read global params from config file
     sr = CONFIG.DATA.sr                        # 48000
@@ -56,7 +53,6 @@ def main():
         device='cpu'
     )
 
-    # file 23 {'Intrusive': 1.6867953538894653, 'Non-intrusive': 1.6867953538894653, 'LSD': 2.100722, 'STOI': tensor(0.8870), 'PESQ': tensor(1.5858), 'SPEECHMOS': 2.4181840737660725}
     file_name = 'plcchallenge2024_val_0256.wav'
     print('File_name:', file_name)
     file_path = os.path.join(audio_test_folder, file_name)
@@ -72,7 +68,7 @@ def main():
     y_ref, sr = librosa.load(file_path, sr=sr, mono=True)
 
     # Simulate packet losses 
-    y_lost = simulate_packet_loss(y_ref, trace, packet_dim)   # packet dim = 320, il + 80 fatto dopo con fadeout (prima era extra_dim)
+    y_lost = simulate_packet_loss(y_ref, trace, packet_dim)  
     # Predict using PARCnet
     y_pred = parcnet(y_lost, trace)
     
@@ -101,8 +97,7 @@ def main():
     # PESQ
     pesq = pesq_metric(torch.tensor(y_pred_res), torch.tensor(y_ref_res))    
     
-    # print('stoi:', stoi, 'lsd:', lsd, 'plcmos:', ret, 'speechmos:', plcmos_v2_value, 'pesq:', pesq)
-    print( 'plcmos:', ret, 'speechmos:', plcmos_v2_value, 'pesq:', pesq)
+    print('stoi:', stoi, 'lsd:', lsd, 'plcmos:', ret, 'speechmos:', plcmos_v2_value, 'pesq:', pesq)
 
     y_pred_save = y_pred.copy()
     normalized_array = np.clip(y_pred_save, -1, 1)
