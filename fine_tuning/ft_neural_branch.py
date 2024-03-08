5e479f0c60703b0b1774c7488a22b9f95bf86a61
Marco9723 <82958588+Marco9723@users.noreply.github.com>
2024-03-08 17:09:30 +0100

    Update ft_neural_branch.py

--- fine_tuning/ft_neural_branch.py
+++ fine_tuning/ft_neural_branch.py
@@ -8,7 +8,6 @@ from torch import nn
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import numpy as np
-sys.path.append("/nas/home/mviviani/nas/home/mviviani/tesi")
 from CODE import metr
 from CODE.config import CONFIG
 from CODE.blocks import Encoder, Predictor
@@ -16,13 +15,12 @@ from CODE.utils import visualize, LSD
 from CODE.ar_branch import ARModel
 from CODE.parcnet_loss import SpectralConvergenceLoss, SingleResolutionSTFTLoss, LogMagnitudeSTFTLoss, _spectrogram, \
     MultiResolutionSTFTLoss
-
+sys.path.append("/nas/home/mviviani/nas/home/mviviani/tesi")
 
 class FinePLCModel(pl.LightningModule):
     def __init__(self, train_dataset=None, val_dataset=None, window_size=960, enc_layers=4, enc_in_dim=384, enc_dim=768,
-                 pred_dim=512, pred_layers=1, model=None, pred_ckpt_path=None): # fine_tune=False, fine_tune_ckpt_path=None):
-        super(FinePLCModel, self).__init__()       
-        # pred_ckpt_path='lightning_logs/predictor/checkpoints/predictor.ckpt'
+                 pred_dim=512, pred_layers=1, model=None, pred_ckpt_path=None): 
+        super(FinePLCModel, self).__init__()     
         self.pretrained_model = model
         self.val_predictions = []
         self.train_predictions = []
@@ -41,22 +39,11 @@ class FinePLCModel(pl.LightningModule):
         self.val_dataset = val_dataset
         self.lmbda = 100.0
 
-        # self.train_dataset.previous_predictions = None
-        # self.val_dataset.previous_predictions = None
-        # if fine_tune:
-        # Se si sta facendo il fine-tuning, carica i pesi del modello pre-allenato
-        #    self.load_fine_tune_weights(fine_tune_ckpt_path)
-            
-        # RIMETTI POI IF ELSE PER USARE ANCHE MODELLO PREALLENATO DEL PREDICTOR  -
-        # self.predictor = Predictor(window_size=self.window_size, lstm_dim=self.pred_dim,
-        #                           lstm_layers=self.pred_layers)
-
         self.window = torch.sqrt(torch.hann_window(self.window_size)).to('cuda:0')
         self.save_hyperparameters('window_size', 'enc_layers', 'enc_in_dim', 'enc_dim', 'pred_dim', 'pred_layers')
         self.mse_loss = F.mse_loss
         self.stft_loss = MultiResolutionSTFTLoss()
         
-
     def forward(self, x):
         output = self.pretrained_model(x)
         return output
@@ -69,11 +56,10 @@ class FinePLCModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         ar_past, nn_past, target = batch
 
+        context = ar_past
         f_0 = nn_past[:, :, 0:1, :]
         nn_input = nn_past[:, :, 1:, :]
-
         nn_pred = self(nn_input)
-        
         nn_pred = torch.cat([f_0, nn_pred], dim=2)
         nn_pred = torch.view_as_complex(nn_pred.permute(0, 2, 3, 1).contiguous())
         nn_pred = torch.istft(nn_pred, self.window_size, self.hop_size, window=self.window)
@@ -85,14 +71,18 @@ class FinePLCModel(pl.LightningModule):
             prediction = self.ar_model.predict(ar_past[i], (self.p_size + self.fadeout + self.padding))
             tmp = np.concatenate((ar_past[i], prediction), axis=None)
             ar_pred.append(tmp)
+ 
+        ar_pred = torch.tensor(np.array(ar_pred))
 
-        ar_pred = torch.tensor(np.array(ar_pred))    
+        nn_pred = nn_pred[:, 7 * self.p_size:]
+        ar_pred = ar_pred[:, 7 * self.p_size:]
         pred = nn_pred.to('cuda:0') + ar_pred.to('cuda:0')
+        output = pred[:, :self.p_size]   # tensor(20,320)
+        pred = torch.cat((context, pred), dim=1)
 
-        output = pred[:, 7 * self.p_size : (7 + 1) * self.p_size]   # tensor(20,320)
-        self.train_predictions.append(output)   # dovrebbe farlo in ordine
+        self.train_predictions.append(output) 
 
-        pred = pred.to(torch.double)        # <-------- PROBABILMENTE SBAGLIATO CALCOLARE LE LOSS COME PRIMA
+        pred = pred.to(torch.double)     
         target = target.to(torch.double)
         
         mse_loss = self.mse_loss(pred, target)  
@@ -110,25 +100,26 @@ class FinePLCModel(pl.LightningModule):
         return tot_loss
 
     def on_train_epoch_end(self):
-        if self.current_epoch % 8 == 0:   # 0, 8, 16, 24: è VUOTA, appendo PER LA PROSSIMA
+        if self.current_epoch % 8 == 0:   
             self.train_dataset.previous_predictions = torch.cat(self.train_predictions, dim=0).detach().cpu().numpy()
-        elif self.current_epoch % 8 == 7:   # 7, 15, 23, 31  svuoto, in quest'epoca ho già lavorato con 7 pacchetti
+        elif self.current_epoch % 8 == 7:  
             self.train_dataset.previous_predictions = None
+            self.train_dataset.audio_index = torch.randint(0, CONFIG.DATA.audio_chunk_len - self.p_size*17, (1,))[0]
         else:
             self.train_dataset.previous_predictions = np.concatenate((self.train_dataset.previous_predictions, torch.cat(self.train_predictions, dim=0).detach().cpu().numpy()), axis=1)
         self.train_predictions.clear()
         self.train_predictions = []
-        # print('train:', self.train_dataset.previous_predictions.shape)
 
     def validation_step(self, val_batch, batch_idx):
         ar_past, nn_past, target = val_batch
 
+        context = ar_past
         f_0 = nn_past[:, :, 0:1, :]
-        nn_input = nn_past[:, :, 1:, :]   # torch.Size([20, 2, 480, 7]) 
-        nn_pred = self(nn_input)          # torch.Size([20, 2, 480, 7])
-        nn_pred = torch.cat([f_0, nn_pred], dim=2)   # torch.Size([20, 2, 481, 7])
+        nn_input = nn_past[:, :, 1:, :]              
+        nn_pred = self(nn_input)                     
+        nn_pred = torch.cat([f_0, nn_pred], dim=2)   
         nn_pred = torch.view_as_complex(nn_pred.permute(0, 2, 3, 1).contiguous())
-        nn_pred = torch.istft(nn_pred, self.window_size, self.hop_size, window=self.window) # torch.Size([20, 2880])
+        nn_pred = torch.istft(nn_pred, self.window_size, self.hop_size, window=self.window)               # torch.Size([20, 2880])
 
         ar_pred = []
         ar_past = ar_past.cpu().numpy()
@@ -138,32 +129,34 @@ class FinePLCModel(pl.LightningModule):
             tmp = np.concatenate((ar_past[i], prediction), axis=None)
             ar_pred.append(tmp)
 
-        ar_pred = torch.tensor(np.array(ar_pred))
-        pred = nn_pred.to('cuda:0') + ar_pred.to('cuda:0')   # 9
+        ar_pred = torch.tensor(np.array(ar_pred))            # 9
 
-        output = pred[:, 7 * self.p_size: (7 + 1) * self.p_size]
+        nn_pred = nn_pred[:, 7 * self.p_size:]               # 640
+        ar_pred = ar_pred[:, 7 * self.p_size:]               # 640 
+        pred = nn_pred.to('cuda:0') + ar_pred.to('cuda:0')   # 640
+        output = pred[:, :self.p_size]                       # 320   
+        pred = torch.cat((context, pred), dim=1)
 
         self.val_predictions.append(output)
 
-        val_loss = metr.nmse(y_pred=pred, y_true=target)  # 2880
-        packet_val_loss = metr.nmse(y_pred=pred[..., -(self.p_size + self.fadeout + self.padding):], y_true=target[..., -(self.p_size + self.fadeout + self.padding):])   # ultimi 640
+        val_loss = metr.nmse(y_pred=pred, y_true=target)    # 2880
+        packet_val_loss = metr.nmse(y_pred=pred[..., -(self.p_size + self.fadeout + self.padding):], y_true=target[..., -(self.p_size + self.fadeout + self.padding):])   # last 640
 
-        self.log('val_loss', val_loss)   # <--------  NEL FINE TUNING FORSE NON SERVE, MA FORSE ANCHE NEL TRAINING ORIGINALE
+        self.log('val_loss', val_loss)  
         self.log('packet_val_loss', packet_val_loss)
 
-        return packet_val_loss, val_loss   # val_loss, packet_val_loss   # <--------
+        return packet_val_loss, val_loss   
     
     def on_validation_epoch_end(self):
         if self.current_epoch % 8 == 0:
             self.val_dataset.previous_predictions = torch.cat(self.val_predictions, dim=0).detach().cpu().numpy()
         elif self.current_epoch % 8 == 7:
             self.val_dataset.previous_predictions = None
+            self.val_dataset.audio_index = torch.randint(0, CONFIG.DATA.audio_chunk_len - self.p_size * 17, (1,))[0]
         else:
             self.val_dataset.previous_predictions = np.concatenate((self.val_dataset.previous_predictions, torch.cat(self.val_predictions, dim=0).detach().cpu().numpy()), axis=1)
-            # self.val_dataset.previous_predictions = np.hstack((self.val_dataset.previous_predictions, torch.cat(self.val_predictions, dim=0).detach().cpu().numpy()))
         self.val_predictions.clear()
         self.val_predictions = []
-        # print('validation:', len(self.val_dataset.previous_predictions))
 
     def save_checkpoint(self, checkpoint_path='fine_tuned_model.ckpt'):
         torch.save(self.state_dict(), checkpoint_path)
@@ -174,16 +167,10 @@ class FinePLCModel(pl.LightningModule):
         lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=CONFIG.TRAIN.patience,
                                                                   factor=CONFIG.TRAIN.factor, verbose=True)
 
-        '''scheduler = {
-            'scheduler': lr_scheduler,
-            'reduce_on_plateau': True,        # <--------   CAMBIATO PER MONITORARE VAL LOSS, SE NO RIMETTI
-            'monitor': 'val_loss'
-        }
-        return [optimizer], [scheduler]'''
-
         scheduler = {
             'scheduler': lr_scheduler,
             'reduce_on_plateau': True,
             'monitor': 'packet_val_loss'
         }
         return [optimizer], [scheduler]
+
