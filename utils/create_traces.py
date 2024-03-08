7f24a7a44604591cb2046235a39cfac55a50e4c1
Marco9723 <82958588+Marco9723@users.noreply.github.com>
2024-03-08 16:31:14 +0100

    Update create_traces.py

--- utils/create_traces.py
+++ utils/create_traces.py
@@ -1,11 +1,9 @@
 import os
 import csv
 
-# Leggi il file Excel e crea i file di testo
-csv_file_path = 'C:/Users/marco/Desktop/plcchallenge24_val_v2/val_metal.csv'  # Sostituisci con il percorso del tuo file Excel
-output_folder = 'C:/Users/marco/Desktop/traces_v2'  # Sostituisci con il nome della cartella in cui desideri salvare i file di testo
+csv_file_path = 'C:/Users/marco/Desktop/plcchallenge24_val_v2/val_meta.csv' 
+output_folder = 'C:/Users/marco/Desktop/traces_v2'  
 
-# Crea la cartella di output se non esiste già
 os.makedirs(output_folder, exist_ok=True)
 
 with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
@@ -14,15 +12,12 @@ with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
         if len(row) >= 4:
             sequence = row[3].replace(" ", "").strip()
             
-            # Ottieni il nome del file senza l'estensione ".wav"
             file_name = row[0].split(".wav")[0]
             
-            # Crea il nome del file di testo
             output_file_name = f"{file_name.replace(' ', '_')}.txt"
             output_file_path = os.path.join(output_folder, output_file_name)
 
-            # Scrivi ogni cifra della sequenza su una riga nel file di testo senza spazi tra le righe
             with open(output_file_path, 'w') as output_file:
                 output_file.write('\n'.join(sequence))
 
-print("Operazione completata. I file di testo sono stati creati nella cartella:", output_folder)
+print("Saved in:", output_folder)
