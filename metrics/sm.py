13dc6c686a70024276bc427dc60e5632538f86e9
Marco9723 <82958588+Marco9723@users.noreply.github.com>
2024-03-08 16:56:34 +0100

    Update sm.py

--- metrics/sm.py
+++ metrics/sm.py
@@ -1,16 +1,6 @@
 import librosa
 from speechmos import plcmos 
-#from config import CONFIG
 import os
-
-'''def load_txt(txt_list):
-    target = []
-    with open(txt_list) as f:
-        for line in f:
-            target.append(os.path.join('C:/Users/marco/Documents/GitHub/FNR/data/', line.strip('\n')))
-    target = list(set(target))
-    target.sort()
-    return target'''
 
 def load_file_list(file_path):
     file_list = []
@@ -29,18 +19,13 @@ def load_file_list(file_path):
 def calculate_speechmos_for_files(file_list):
     for file_path in file_list:
         path=os.path.join("C:/Users/marco/Documents/GitHub/FNR/data/plcchallenge/", file_path)
-        #print(path)
         plcmos_v2=plcmos.run(path, sr=16000)  
-        print(plcmos_v2) 
-        #print(f"File: {path} - Speechmos: {plcmos_v2:.2f} ")   #<-----
+        print('Speechmos':,plcmos_v2) 
 
-#data_list contiene cose tipo: 'plcchallenge2024_val/val_clean/plcchallenge2024_val_0758.wav'         
+# 'plcchallenge2024_val/val_clean/plcchallenge2024_val_0758.wav'         
 path_to_txt='C:/Users/marco/Documents/GitHub/FNR/data/test.txt'
 data_list = load_file_list(path_to_txt)
 calculate_speechmos_for_files(data_list)
 
-#Lui funziona:
-#plc_mos=plcmos.run("C:/Users/marco/Documents/GitHub/FNR/data/plcchallenge/plcchallenge2024_val/val_lossy/plcchallenge2024_val_0000.wav", sr=16000) 
-#print(plc_mos)
 
         
