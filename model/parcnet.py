4b45dfb82ff97a1f96bf21c69cd4e0874d411bfe
Marco9723 <82958588+Marco9723@users.noreply.github.com>
2024-03-08 16:53:51 +0100

    Update parcnet.py

--- model/parcnet.py
+++ model/parcnet.py
@@ -6,9 +6,9 @@ from tqdm import tqdm
 from copy import deepcopy
 from ar_branch import ARModel
 from neural_branch import PLCModel
+from CODE.fine_tuning.ft_neural_branch import FinePLCModel 
+from config import CONFIG 
 sys.path.append("C:/Users/marco/Documents/GitHub/Packet_Loss_Concealment_Thesis/TESI")
-from CODE.fine_tuning.ft_neural_branch import FinePLCModel  # <----------
-from config import CONFIG                                   # <----------
 
 class PARCnet:
 
@@ -18,14 +18,14 @@ class PARCnet:
                  padding: int,   # 240
                  ar_order: int,
                  ar_diagonal_load: float,
-                 num_valid_ar_packets: int,   # sarebbe 10 ma fatto con 7
+                 num_valid_ar_packets: int,   # 7 packets
                  num_valid_nn_packets: int,
                  model_checkpoint: str,
                  xfade_len_in: int,
                  device: str = 'cpu',
                  ):
 
-        self.packet_dim = packet_dim        # 320        NB: fadeout e fade_out
+        self.packet_dim = packet_dim        # 320       
         self.f_out = fadeout                # 80
         self.extra_dim = padding            # 240 
 
@@ -49,7 +49,7 @@ class PARCnet:
         self.ar_model = ARModel(ar_order, ar_diagonal_load)
 
         # Load the pretrained neural network
-        self.neural_net = PLCModel.load_from_checkpoint(model_checkpoint, channels=1, lite=True).to(device)  # dopo format va, dovrebbe essere giusto
+        self.neural_net = PLCModel.load_from_checkpoint(model_checkpoint, channels=1, lite=True).to(device) 
         
         self.hann = torch.sqrt(torch.hann_window(960)) 
 
@@ -63,7 +63,7 @@ class PARCnet:
                 idx = i * self.packet_dim   # 320
 
                 # AR model context
-                valid_ar_packets = output_signal[idx - self.ar_context_len:idx]   # 7 x 320, prima faceva 10
+                valid_ar_packets = output_signal[idx - self.ar_context_len:idx]   # 7 x 320
 
                 # AR model inference
                 # print(len(valid_ar_packets))  <--- 1440, 640, sempre 2240 in teoria
@@ -71,41 +71,38 @@ class PARCnet:
 
                 # NN model context
                 nn_context = output_signal[idx - self.nn_context_len: idx]    # 7 x 320 
-                # ATTENZIONE QUA, NON PROPRIO COME DATASET
-                nn_context = np.pad(nn_context, (0, self.pred_dim + self.extra_dim))   # nel nn paddi per 2 packets da 320 (400 + 240)
-                nn_context = torch.tensor(nn_context)   # torch.Size([2880]) come in dataset       
+                nn_context = np.pad(nn_context, (0, self.pred_dim + self.extra_dim))   # padding (400 + 240)
+                nn_context = torch.tensor(nn_context)                                  # 2880       
                 nn_context = torch.stft(nn_context, 960, 480, window=self.hann, return_complex=False).permute(2, 0, 1).float()
-                nn_context = nn_context.unsqueeze(0)  # aggiungi una dimensione, in teoria giust perchè sono 3 e aggiungo batch size all'inizio
+                nn_context = nn_context.unsqueeze(0)  
                 
                 with torch.no_grad():
                     # NN model inference
                     f_0 = nn_context[:, :, 0:1, :]   
                     nn_input = nn_context[:, :, 1:, :]  
                     nn_pred = self.neural_net(nn_input)   
-                    nn_pred = torch.cat([f_0, nn_pred], dim=2)   # torch.Size([20, 2, 481, 7])
+                    nn_pred = torch.cat([f_0, nn_pred], dim=2)   
                     nn_pred = torch.view_as_complex(nn_pred.permute(0, 2, 3, 1).contiguous())
-                    nn_pred = torch.istft(nn_pred, 960, 480, window=self.hann)  # 2240 + 320 + 320 = 2880   torch.Size([1, 2880])
-                    nn_pred = nn_pred[..., -(self.pred_dim + self.extra_dim):]  # prendi ultimi 320 + 320 
-                    nn_pred = nn_pred[..., :self.pred_dim]   # primi 400 (320 + 80)   torch.Size([1, 400])
-                    nn_pred = nn_pred.squeeze().cpu().numpy()  # [400]
+                    nn_pred = torch.istft(nn_pred, 960, 480, window=self.hann)  # 2240 + 320 + 320 = 2880  
+                    nn_pred = nn_pred[..., -(self.pred_dim + self.extra_dim):]  # last 320 + 320 
+                    nn_pred = nn_pred[..., :self.pred_dim]   # first 400 (320 + 80)   
+                    nn_pred = nn_pred.squeeze().cpu().numpy()  # 400
                     
                 # Apply fade-in to the neural network contribution (inbound fade-in)
                 nn_pred *= self.fade_in  # 400
 
                 # Combine the two predictions
                 prediction = ar_pred + nn_pred  # 400
-                prediction = np.clip(prediction, -1e15, 1e15)
+                # prediction = np.clip(prediction, -1e15, 1e15)
 
                 # Cross-fade the compound prediction (outbound fade-out)
                 prediction *= self.fade_out  # 400
                 
                 # Cross-fade the output signal (outbound fade-in)
-                # output_signal[idx:idx + self.pred_dim] *= 1 - self.fade_out
                 chunk_len = len(output_signal[idx:idx + self.pred_dim])  # 400
                 output_signal[idx:idx + self.pred_dim] *= 1 - self.fade_out[:chunk_len]   
                 
                 # Conceal lost packet
-                # output_signal[idx: idx + self.pred_dim] += prediction
                 output_signal[idx: idx + self.pred_dim] += prediction[:chunk_len]
 
         return output_signal
